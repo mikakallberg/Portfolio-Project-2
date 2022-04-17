4dafc4a987f3c3aee7cf6c510a2a2a9818c1d972
mikakallberg <mikaela.kallberg@hotmail.com>
2022-04-17 11:42:25 +0000

    Add first elements to JavaScript, getElements

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//Elements affected by JavaScript
+const question = document.getElementById(#question-area);
+const options = document.getElementsByClassName(.options);
+const scores = document.getElementsByClassName(.scores);
+
+//Baseline for JavScript
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0; 
+let availableQuestions = [];
+
+
+
