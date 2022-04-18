3f64bd29c690bd7f267f1c375657384a9dfef967
mikakallberg <mikaela.kallberg@hotmail.com>
2022-04-18 15:31:24 +0000

    Trouble shooting

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,30 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 })
 
+let myQuestions = [
+    {
+        questionArea: 'Which option contains organelles?',
+        optionOne: 'Nucleus, Mitochondrion, Rough endoplasmic reticulum, Maxilla',
+        optionTwo: 'Mitochondrion, Nucleus, Proteasome, Peroxisome',
+        optionThree: 'Smooth endoplasmic reticulum, Ribosome, Nucleus, Epicondyle',
+        optionFour: 'Golgi complex, Ribosome, Miochondrion, Ulna',
+        correctAnswer: 'Mitochondrion, Nucleus, Proteasome, Peroxisome',
+    },
+    {
+        questionArea: 'Which protein is NOT a part of the cellmembrane?',
+        optionOne: 'Ion channel',
+        optionTwo: 'Enzyme',
+        optionThree: 'Linker',
+        optionFour: 'Transponder',
+        correctAnswer: 'Transponder',
+    },
+   
+];
+
+
 function startQuiz(){
-    let questions = Math.floor(Math.random() * questions.length);
+    let questions = Math.floor(Math.random() * myQuestions.length);
+    console.log("question:" , questions)
 }
 
 function displayQuestion(){
@@ -42,3 +64,4 @@ function calculateScore(){
 
 }
 
+startQuiz()
