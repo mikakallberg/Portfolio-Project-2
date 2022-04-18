989a8d1f7d49ebc82aa7cfa20b6a3c28718c3718
mikakallberg <mikaela.kallberg@hotmail.com>
2022-04-18 11:21:39 +0000

    Add height and width to content on quiz page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,16 @@ const optionTwo = document.getElementById('#option-two');
 const optionThree = document.getElementById('#option-three');
 const optionFour = document.getElementById('#option-four');
 const scoresBtn = document.getElementsByClassName('.score-btn');
+//After game player enters username to save score to resultpage
+const username = document.getElementById('start-game');
+//
 
-//Quiz
-function results (){}
+
 //scores.addEventlistener('Click', showResults);<----MUST FIX
 
 //Baseline for JavaScript
 let currentQuestion = {};
-let acceptingAnswers = true;
+let correctAnswer = true;
 let score = 0;
 let questionCounter = 0; 
 let availableQuestions = [];
@@ -24,23 +26,19 @@ let availableQuestions = [];
 
 let myQuestions = [
     {
-        questions: 'Which option contains organelles?',
-        options{
-            optionOne: 'Nucleus, Mitochondrion, Rough endoplasmic reticulum, Maxilla',
-            optionTwo: 'Mitochondrion, Nucleus, Proteasome, Peroxisome',
-            optionThree: 'Smooth endoplasmic reticulum, Ribosome, Nucleus, Epicondyle',
-            optionFour: 'Golgi complex, Ribosome, Miochondrion, Ulna'
-        },
+        questionArea: 'Which option contains organelles?',
+        optionOne: 'Nucleus, Mitochondrion, Rough endoplasmic reticulum, Maxilla',
+        optionTwo: 'Mitochondrion, Nucleus, Proteasome, Peroxisome',
+        optionThree: 'Smooth endoplasmic reticulum, Ribosome, Nucleus, Epicondyle',
+        optionFour: 'Golgi complex, Ribosome, Miochondrion, Ulna'
         correctAnswer: 'Mitochondrion, Nucleus, Proteasome, Peroxisome',
     },
     {
-        questions: 'Which protein is NOT a part of the cellmembrane?',
-        options{
-            optionOne: 'Ion channel',
-            optionTwo: 'Enzyme',
-            optionThree: 'Linker',
-            optionFour: 'Transponder',
-        }
+        questionArea: 'Which protein is NOT a part of the cellmembrane?',
+        optionOne: 'Ion channel',
+        optionTwo: 'Enzyme',
+        optionThree: 'Linker',
+        optionFour: 'Transponder',
         correctAnswer: 'Transponder',
     },
    
@@ -59,7 +57,7 @@ function startGame = () =>{
 function getNewQuestion = () =>{
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score)
-        return window.location.assign('/end.html')
+        return window.location.assign('/result.html')
     }
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length)
