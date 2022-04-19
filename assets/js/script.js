1e34a33ed6096ac9ef17ff2d846f267366aa6dde
mikakallberg <mikaela.kallberg@hotmail.com>
2022-04-19 08:01:21 +0000

    Add in rest of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,6 +42,174 @@ const myQuestions = [
         }
         ]
      },
+     {
+        id: 2,
+        questionArea: 'In which solution does red blood cells swell?',
+        answers: [{
+            text: 'Isotonic solution',
+            isCorrect: false
+        },
+        {
+            text: 'Hypertonic solution',
+            isCorrect: false
+        },
+        {
+            text: 'Hypotonic soluton',
+            isCorrect: true
+        },
+        {
+            text: 'Schweppes solution',
+            isCorrect: false
+        }
+        ]
+     },
+     {
+        id: 3,
+        questionArea: 'What is the name of the longest nerve in the body?',
+        answers: [{
+            text: 'The sciatic nerve',
+            isCorrect: true
+        },
+        {
+            text: 'The median nerve',
+            isCorrect: false
+        },
+        {
+            text: 'The femoral nerve',
+            isCorrect: false
+        },
+        {
+            text: 'The ischias nerve',
+            isCorrect: false
+        }
+        ]
+     },
+     {
+        id: 4,
+        questionArea: 'What is the blood vessel called that transports blood FROM the heart to the lungs?',
+        answers: [{
+            text: 'Aorta',
+            isCorrect: false
+        },
+        {
+            text: 'Vena cava',
+            isCorrect: false
+        },
+        {
+            text: 'Pulmonary artery',
+            isCorrect: true
+        },
+        {
+            text: 'Pulmonary veins',
+            isCorrect: false
+        }
+        ]
+     },
+     {
+        id: 5,
+        questionArea: 'What are the smallest blood vessels called?',
+        answers: [{
+            text: 'arteriols and veins',
+            isCorrect: false
+        },
+        {
+            text: 'aorta and vena cava',
+            isCorrect: false
+        },
+        {
+            text: 'capillaries and venoles',
+            isCorrect: true
+        },
+        {
+            text: 'capillaries and veins',
+            isCorrect: false
+        }
+        ]
+     },
+     {
+        id: 6,
+        questionArea: 'Which is the bodies biggest organ?',
+        answers: [{
+            text: 'Liver',
+            isCorrect: false
+        },
+        {
+            text: 'Lungs',
+            isCorrect: false
+        },
+        {
+            text: 'Skin',
+            isCorrect: true
+        },
+        {
+            text: 'Small intestines',
+            isCorrect: false
+        }
+        ]
+     },
+     {
+        id: 7,
+        questionArea: 'Which is the bodies biggest internal organ?',
+        answers: [{
+            text: 'Liver',
+            isCorrect: true
+        },
+        {
+            text: 'Stomach',
+            isCorrect: false
+        },
+        {
+            text: 'Colon',
+            isCorrect: false
+        },
+        {
+            text: 'Esophagus',
+            isCorrect: false
+        }
+        ]
+     },
+     {
+        id: 8,
+        questionArea: 'What is the area called where urine is first collected?',
+        answers: [{
+            text: 'Urinary bladder',
+            isCorrect: false
+        },
+        {
+            text: 'Ureter',
+            isCorrect: false
+        },
+        {
+            text: 'Nephron',
+            isCorrect: false
+        },
+        {
+            text: 'Renal pelvis',
+            isCorrect: true
+        }
+        ]
+     },
+     {
+        id: 9,
+        questionArea: 'What is the structure called that filtrates the blood and produces urine?',
+        answers: [{
+            text: 'Nephrone',
+            isCorrect: true
+        },
+        {
+            text: 'Arterioles',
+            isCorrect: false
+        },
+        {
+            text: 'Neurones',
+            isCorrect: false
+        },
+        {
+            text: 'Renal ganglion',
+            isCorrect: false
+        }
+        ]
+     },
     
    
 ];
