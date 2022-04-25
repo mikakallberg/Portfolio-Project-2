b478879d487023c791f7d084c1c5cd81249f4b7f
mikakallberg <mikaela.kallberg@hotmail.com>
2022-04-25 08:32:00 +0000

    Remove commented out text in script.js and add comments to code in script.js, and ad variable for score and counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-
+//Questions for the game, different properties (id) are connected to different numbers, which are connected to questions, answers and a boolean that states if the string is correct or not.
 const myQuestions = [
     {
         id: 0,
@@ -214,8 +214,12 @@ const myQuestions = [
    
 ]; 
 
+//Variable with a boolean that states that the game starts from the outset.
 let start = true;
+//Score variable and counter variable, they both start at zero.
+let score = 0;
 var counter = 0;
+
 //Where is iterateQuestions declared?
 function iterateQuestions(id){
     //get result
@@ -346,164 +350,3 @@ function iterateQuestions(id){
 
 if (start){
     iterateQuestions('0');}
-/**    
-
-
-   function iterateQuestions(id){
-    //get result
-
-    //Get the area where questions are to be displayed
-    const question = document.getElementById('question-area');
-
-    //Get the text from the array for the question-area
-    question.innerText = myQuestions[id].questionArea;
-
-    //Get the four areas where options are to be displayed
-    const option1 = document.getElementById('option-one');
-    const option2 = document.getElementById('option-two');
-    const option3 = document.getElementById('option-three');
-    const option4 = document.getElementById('option-four');
-
-    //Get the text for the options from the array
-    option1.innerText = myQuestions[id].answers[0].text;
-    option2.innerText = myQuestions[id].answers[1].text;
-    option3.innerText = myQuestions[id].answers[2].text;
-    option4.innerText = myQuestions[id].answers[3].text;
-
-    
-    }
-
-
-   const next = document.getElementsByClassName('next')[0];
-   var counter = 0;
-   var selected = '';
-
-   function selected(){
-    //Respond with true or false value to each answer, SHOULD THIS BE MOVED TO FUNCTION BELOW?
-    option1.value = myQuestions[id].answers[0].isCorrect;
-    option2.value = myQuestions[id].answers[1].isCorrect;
-    option3.value = myQuestions[id].answers[2].isCorrect;
-    option4.value = myQuestions[id].answers[3].isCorrect;
-   //Give response to selection for option1
-    option1.addEventListener('click', () => {
-        option1.style.backgroundColor = '#527c88';
-        option2.style.backgroundColor = '#d7baad';
-        option3.style.backgroundColor = '#d7baad';
-        option4.style.backgroundColor = '#d7baad';
-        selected = option1.value;
-        console.log(option1.value);
-        if(selected == true){
-            result[counter].innerHTML = '<i class="fa-solid fa-check"></i>';
-            result[counter].style.backgroundColor = "#00cc99";
-            result[counter].style.border = "#009973 solid";
-        }else{
-            result[counter].innerHTML = '<i class="fa-solid fa-x"></i>';
-            result[counter].style.backgroundColor = "#ff6666";
-            result[counter].style.border = "#cc0000 solid";
-        }
-    });
-    //Give response to selection for option2
-    option2.addEventListener('click', () => {
-        option1.style.backgroundColor = '#d7baad';
-        option2.style.backgroundColor = '#527c88';
-        option3.style.backgroundColor = '#d7baad';
-        option4.style.backgroundColor = '#d7baad';
-        selected = option2.value;
-        console.log(option2.value);
-        if(selected == true){
-            result[counter].innerHTML = '<i class="fa-solid fa-check"></i>';
-            result[counter].style.backgroundColor = "#00cc99";
-            result[counter].style.border = "#009973 solid";
-        }else{
-            result[counter].innerHTML = '<i class="fa-solid fa-x"></i>';
-            result[counter].style.backgroundColor = "#ff6666";
-            result[counter].style.border = "#cc0000 solid";
-        }
-    });
-    //Give response to selection for option3
-    option3.addEventListener('click', () => {
-        option1.style.backgroundColor = '#d7baad';
-        option2.style.backgroundColor = '#d7baad';
-        option3.style.backgroundColor = '#527c88';
-        option4.style.backgroundColor = '#d7baad';
-        selected = option3.value;
-        console.log(option3.value);
-        if(selected == true){
-            result[counter].innerHTML = '<i class="fa-solid fa-check"></i>';
-            result[counter].style.backgroundColor = "#00cc99";
-            result[counter].style.border = "#009973 solid";
-        }else{
-            result[counter].innerHTML = '<i class="fa-solid fa-x"></i>';
-            result[counter].style.backgroundColor = "#ff6666";
-            result[counter].style.border = "#cc0000 solid";
-        }
-    });
-    //Give response to selection for option4
-    option4.addEventListener('click', () => {
-        option1.style.backgroundColor = '#d7baad';
-        option2.style.backgroundColor = '#d7baad';
-        option3.style.backgroundColor = '#d7baad';
-        option4.style.backgroundColor = '#527c88';
-        selected = option4.value;
-        console.log(option4.value);
-        if(selected == true){
-            console.log('resultcounter: ', result[counter].innerHTML);
-            result[counter].innerHTML = '<i class="fa-solid fa-check"></i>';
-            result[counter].style.backgroundColor = "#00cc99";
-            result[counter].style.border = "#009973 solid";
-        }else{
-            result[counter].innerHTML = '<i class="fa-solid fa-x"></i>';
-            result[counter].style.backgroundColor = "#ff6666";
-            result[counter].style.border = "#cc0000 solid";
-        }
-    });
-}
-
-   function score (){
-   const next = document.getElementsByClassName('next')[0];
-    /**var id = 0;
-
-    next.addEventListener('click', () => {
-        option1.style.backgroundColor = '#d7baad';
-        option2.style.backgroundColor = '#d7baad';
-        option3.style.backgroundColor = '#d7baad';
-        option4.style.backgroundColor = '#d7baad';
-        start = false;
-    if (id < 10){
-        id++;
-        iterateQuestions(id);
-        console.log(id);
-    }
-    if (result === counter || result < 10){
-        counter++;
-        iterateQuestions(id);
-        console.log(result);
-    }
-    });
-}
-
-if (start){
-    iterateQuestions('0');}*/ 
- 
-
-
-
-/**gameOver function
- * triggers username input-element
- * value from username input-element and result from number of selected === true
- * displays on result.html id 'score-list'
- */
-/**let score = result[10]
-
-function score(counter, id.length){
-    var score = `${(counter/ id.length) * 100}% //calculate score in percentage
-};*/ //working on gameOver function
-
-/** 
-
-function calculateScore(){
-
-}
-
-startQuiz()
-*/
